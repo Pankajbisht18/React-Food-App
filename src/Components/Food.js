2ebabae68974bf6c325039793214c5aca9ebef32
Pankaj <techpankaj45@gmail.com>
2021-07-06 19:03:03 +0530

    Add search option

--- src/Components/Food.js
+++ src/Components/Food.js
@@ -1,24 +1,58 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import {Link} from 'react-router-dom';
 
 const Food = () => {
     const[Recipe, setRecipe] = useState([]);
-    const[data, setData] = useState([]);
-    const fetchRecipe = () => {
-        fetch('https://api.edamam.com/api/recipes/v2?type=public&q=paneer&app_id=eabc8ee8&app_key=4d98b1b0c4340c48bc68b1b84a839892')
-        .then(res=>res.json())
-        .then(json=>{
-            setRecipe(json.hits)
-            setData(json)
-        })
+    const[Search, setSearch] = useState();
+    const[cuisineType, setCuisineType] = useState('');
+    const handleText = (evt) => {
+        setSearch(evt.target.value);
     }
-    useEffect(()=>{
-        fetchRecipe();
-    },[])
+    const handleCusineType = (evt) => {
+        setCuisineType(evt.target.value);
+    }
+    const handleSearch = () => {
+        if(cuisineType!=='') {
+            fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=${Search}&app_id=eabc8ee8&app_key=4d98b1b0c4340c48bc68b1b84a839892&cuisineType=${cuisineType}`)
+            .then(res=>res.json())
+            .then(json=>{
+                setRecipe(json.hits)
+            })
+        }
+        else{
+            alert("Select Cuisine Type");
+        }
+    }
+    
     console.log(Recipe);
-    console.log(data);
     return(
         <>
+            <div>
+                <input type="text" placeholder="Enter recipe name" onChange={handleText} />
+                <label>cuisine Type</label>
+                <select name="cuisineType" onChange={handleCusineType}>
+                    <option value=""></option>
+                    <option value="American">American</option>
+                    <option value="Asian">Asian</option>
+                    <option value="British">British</option>
+                    <option value="Caribbean">Caribbean</option>
+                    <option value="Central Europe">Central Europe</option>
+                    <option value="Chinese">Chinese</option>
+                    <option value="Eastern Europe">Eastern Europe</option>
+                    <option value="French">French</option>
+                    <option value="Indian">Indian</option>
+                    <option value="Italian">Italian</option>
+                    <option value="Japanese">Japanese</option>
+                    <option value="Kosher">Kosher</option>
+                    <option value="Mediterranean">Mediterranean</option>
+                    <option value="Mexican">Mexican</option>
+                    <option value="Middle Eastern">Middle Eastern</option>
+                    <option value="Nordic">Nordic</option>
+                    <option value="South American">South American</option>
+                    <option value="South East Asian">South East Asian</option>
+                </select>
+                <button onClick={handleSearch}>Search</button>
+            </div>
             <div>
             {
                 Recipe.map((food,id)=>{
--- src/Components/Recipe.js
+++ src/Components/Recipe.js
@@ -1,6 +1,24 @@
+import { useEffect, useState } from "react";
+
 const Recipe = () => {
+    const[data, setData] = useState([]);
+    useEffect(()=>{
+        fetch('https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=eabc8ee8&app_key=4d98b1b0c4340c48bc68b1b84a839892')
+        .then(res=>res.json())
+        .then(json=>setData(json.hits))
+    },[])
+    console.log(data)
     return(
-        <h1>Recipe</h1>
+        <>
+        {
+            data.map((xyz)=>{
+                return(
+                    <p>{xyz.recipe.shareAs}</p>
+                )
+            })
+            
+        }
+        </>
     );
 }
 export default Recipe;
